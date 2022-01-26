cc6103c53bf07866d46b2be1768e714a94170587
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-01-26 22:59:53 +0100

    Add playground.

--- src/playground.ts
+++ src/playground.ts
@@ -22,3 +22,23 @@ on('onClientGameTypeStart', () => {
   exports.spawnmanager.setAutoSpawn(true)
   exports.spawnmanager.forceRespawn()
 });
+
+RegisterCommand("pg", () => {
+  exports.spawnmanager.setAutoSpawnCallback(() => {
+    exports.spawnmanager.spawnPlayer({
+      x: spawnPos[0],
+      y: spawnPos[1],
+      z: spawnPos[2],
+      model: 'a_m_m_skater_01'
+    }, () => {
+      emit('chat:addMessage', {
+        args: [
+          'Welcome to the party!~'
+        ]
+      })
+    });
+  });
+
+  exports.spawnmanager.setAutoSpawn(true)
+  exports.spawnmanager.forceRespawn()
+}, false);
