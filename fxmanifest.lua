621e7294038a95af547a1a48d2067a82bfb59714
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-01-26 22:21:50 +0000

    Fix manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,9 @@
 fx_version 'cerulean'
 game 'rdr3'
 
-author 'An awesome dude'
+author 'mhahze'
 description 'An awesome, but short, description'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 version '1.0.0'
-
-resource_type 'gametype' { name = 'My awesome game type!' }
 
 client_script 'src/playground.js'
