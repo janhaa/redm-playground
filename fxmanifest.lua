4abc06f07e39ea5a6dee07c9de269c1b45187664
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-01-26 23:00:28 +0100

    Add rdr3_warning

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ game 'rdr3'
 
 author 'An awesome dude'
 description 'An awesome, but short, description'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 version '1.0.0'
 
 resource_type 'gametype' { name = 'My awesome game type!' }
