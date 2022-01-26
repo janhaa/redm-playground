5775e97ba4767369f2d7bd426cf8323273b1c3b3
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-01-26 23:08:18 +0100

    Set proper spawnpoint.

--- src/playground.ts
+++ src/playground.ts
@@ -1,7 +1,7 @@
 /// <reference path='../node_modules/@citizenfx/client/natives_universal.d.ts'/>
 /// <reference path='../node_modules/@citizenfx/client/index.d.ts'/>
 
-const spawnPos = [686.245, 577.950, 130.461];
+const spawnPos = [-278.2138, 807.0493, 119.38];
 
 on('onClientGameTypeStart', () => {
   emit('chat:addMessage', {
@@ -34,21 +34,21 @@ RegisterCommand("pg", () => {
       'Welcome to the party!~'
     ]
   })
-  // exports.spawnmanager.setAutoSpawnCallback(() => {
-  //   exports.spawnmanager.spawnPlayer({
-  //     x: spawnPos[0],
-  //     y: spawnPos[1],
-  //     z: spawnPos[2],
-  //     model: 'a_m_m_skater_01'
-  //   }, () => {
-  //     emit('chat:addMessage', {
-  //       args: [
-  //         'Welcome to the party!~'
-  //       ]
-  //     })
-  //   });
-  // });
+  exports.spawnmanager.setAutoSpawnCallback(() => {
+    exports.spawnmanager.spawnPlayer({
+      x: spawnPos[0],
+      y: spawnPos[1],
+      z: spawnPos[2],
+      model: 'a_m_m_skater_01'
+    }, () => {
+      emit('chat:addMessage', {
+        args: [
+          'Welcome to the party!~'
+        ]
+      })
+    });
+  });
 
-  // exports.spawnmanager.setAutoSpawn(true)
-  // exports.spawnmanager.forceRespawn()
+  exports.spawnmanager.setAutoSpawn(true)
+  exports.spawnmanager.forceRespawn()
 }, false);
