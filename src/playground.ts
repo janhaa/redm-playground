15d420dd9a68f464d0d4199fa2c5faf41fd25e5f
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-01-26 23:04:40 +0100

    Less message.

--- src/playground.ts
+++ src/playground.ts
@@ -4,41 +4,51 @@
 const spawnPos = [686.245, 577.950, 130.461];
 
 on('onClientGameTypeStart', () => {
-  exports.spawnmanager.setAutoSpawnCallback(() => {
-    exports.spawnmanager.spawnPlayer({
-      x: spawnPos[0],
-      y: spawnPos[1],
-      z: spawnPos[2],
-      model: 'a_m_m_skater_01'
-    }, () => {
-      emit('chat:addMessage', {
-        args: [
-          'Welcome to the party!~'
-        ]
-      })
-    });
-  });
+  emit('chat:addMessage', {
+    args: [
+      'Welcome to the party!~'
+    ]
+  })
+  // exports.spawnmanager.setAutoSpawnCallback(() => {
+  //   exports.spawnmanager.spawnPlayer({
+  //     x: spawnPos[0],
+  //     y: spawnPos[1],
+  //     z: spawnPos[2],
+  //     model: 'a_m_m_skater_01'
+  //   }, () => {
+  //     emit('chat:addMessage', {
+  //       args: [
+  //         'Welcome to the party!~'
+  //       ]
+  //     })
+  //   });
+  // });
 
-  exports.spawnmanager.setAutoSpawn(true)
-  exports.spawnmanager.forceRespawn()
+  // exports.spawnmanager.setAutoSpawn(true)
+  // exports.spawnmanager.forceRespawn()
 });
 
 RegisterCommand("pg", () => {
-  exports.spawnmanager.setAutoSpawnCallback(() => {
-    exports.spawnmanager.spawnPlayer({
-      x: spawnPos[0],
-      y: spawnPos[1],
-      z: spawnPos[2],
-      model: 'a_m_m_skater_01'
-    }, () => {
-      emit('chat:addMessage', {
-        args: [
-          'Welcome to the party!~'
-        ]
-      })
-    });
-  });
+  emit('chat:addMessage', {
+    args: [
+      'Welcome to the party!~'
+    ]
+  })
+  // exports.spawnmanager.setAutoSpawnCallback(() => {
+  //   exports.spawnmanager.spawnPlayer({
+  //     x: spawnPos[0],
+  //     y: spawnPos[1],
+  //     z: spawnPos[2],
+  //     model: 'a_m_m_skater_01'
+  //   }, () => {
+  //     emit('chat:addMessage', {
+  //       args: [
+  //         'Welcome to the party!~'
+  //       ]
+  //     })
+  //   });
+  // });
 
-  exports.spawnmanager.setAutoSpawn(true)
-  exports.spawnmanager.forceRespawn()
+  // exports.spawnmanager.setAutoSpawn(true)
+  // exports.spawnmanager.forceRespawn()
 }, false);
